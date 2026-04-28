51387acf4734de1c421fa5e3ed5090df9beac6f2
Pavel <p.zagvozdin@dodobrands.io>
2026-04-28 22:22:53 +0200

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let slow = 0
+        let fast = 0
+
+        do {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        } while (slow !== fast)
+
+        slow = 0
+
+        do {
+            slow = nums[slow]
+            fast = nums[fast]
+        }
+        while (slow !== fast)
+
+        return slow
+    }
+}
