77b74f267a52b226c0d9176a7ec371b75f59af72
Pavel <p.zagvozdin@dodobrands.io>
2026-06-14 17:48:05 +0200

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        const result = []
+
+        const backtrack = (index, partSum, partArr) => {
+            if (partSum === target) {
+                result.push([...partArr])
+            }
+
+            if (partSum > target) {
+                return
+            }
+
+            for (let i = index; i < nums.length; i++) {
+                partArr.push(nums[i])
+                backtrack(i, partSum + nums[i], partArr)
+                partArr.pop()
+            }
+        }
+
+        backtrack(0, 0, [])
+
+        return result
+    }
+}
